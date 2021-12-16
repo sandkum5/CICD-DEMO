95972e0951a1a22eadcaa86bd46aa764f1f19876
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2021-12-15 16:53:17 -0800

    Update ntp.tf

--- ntp.tf
+++ ntp.tf
@@ -6,6 +6,6 @@ resource "intersight_ntp_policy" "ntp_policy" {
     timezone    = var.Timezone
     organization {
         object_type = "organization.Organization"
-        selector    = "$filter=Name eq '${var.org_name}'"
+        selector    = "$filter=Name eq '${var.OrgName}'"
     }
 }
